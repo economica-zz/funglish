7cd16c19f500025eedaf2ab7f2d5307020f92afe
hatalife <hatalife@gmail.com>
2012-11-24 10:46:32 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Funglish::Application.config.secret_token = 'c59d2c42a39b01ef07df5d2a966307559004e5a3cc133bc43bcb923e06dbc22dd143246cd71d1baa52e7179222d0bc8596e82f323d9e63a94d100a014a42ed3e'
